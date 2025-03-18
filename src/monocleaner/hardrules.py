7813987d4d3a52759f25424eecb1ef79770ead1c
Marta Bañón <34235767+mbanon@users.noreply.github.com>
2025-03-18 11:31:32 +0100

    Added yue to HR

--- src/monocleaner/hardrules.py
+++ src/monocleaner/hardrules.py
@@ -42,7 +42,7 @@ safe_noise_detection_langs = {"en", "es", "fr", "pl", "de", "it", "pt", "nl", "c
 #similar_pairs = [{"es","ca"}, {"es","gl"}, {"pt","gl"}, {"no","nn"}, {"no", "da"}]
 atilde_langs = {"pt"}
 acumflex_langs = {"cy", "fr", "fa", "it", "pt", "tr", "vi",}
-CJK = {"zh", "ja", "ko"}
+CJK = {"zh", "ja", "ko", "yue"}
 
 
 class Hardrules():
